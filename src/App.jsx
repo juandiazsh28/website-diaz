0d462bec120645e85719e07e411f38d800debcb6
juandiazsh28 <juanjodiazsh28@gmail.com>
2022-11-20 23:31:28 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import reactLogo from "./assets/avatar.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="/">
+          <img src={reactLogo} className="logo avatar" alt="Juan Diaz" />
+        </a>
+      </div>
+      <h1>Coming soon...</h1>
+      <div className="card">
+        <p>Copyright @ Juan Diaz 2022</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
